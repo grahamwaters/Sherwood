b14e17dd677b884e90417bd19b56ee3bbab2208a
Graham Waters <graham.waters.business@gmail.com>
2022-08-18 13:06:02 -0500

    main: Added add record class with additional attrs

--- sherwood.py
+++ sherwood.py
@@ -6,3 +6,22 @@
 
 from config import config
 from signals import signals
+import pandas as pd
+
+class record:
+    crypto_ticker = '' # the ticker for the coin on Robinhood.
+    quantity = 0.0 # the quantity of the coin that was bought/sold.
+    price = 0.0 # the price at which the coin was bought/sold.
+    order_id = '' # Robinhood order ID we create and save for this trade
+    timestamp = '' # the time the trade was made
+    order_type = '' # 'buy' or 'sell'
+    def __init__(self,crypto_ticker,quantity,price,order_id,timestamp,order_type):
+        """
+        Initialize the record class with the crypto ticker, quantity, price, order_id, timestamp, and order_type.
+        """
+        self.crypto_ticker = crypto_ticker
+        self.quantity = float(quantity)
+        self.price = float(price)
+        self.order_id = order_id
+        self.timestamp = timestamp
+        self.order_type = order_type
